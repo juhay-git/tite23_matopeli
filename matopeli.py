cca04d0408861dcc883de314f313402442c53868
user220031 <Eh.Bleh@seamk.fi>
2024-10-02 16:06:17 +0300

    aloitusruutu lisätty

--- matopeli.py
+++ matopeli.py
@@ -20,10 +20,19 @@ class SnakeGame(QGraphicsView):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_game)
 
-        self.start_game()
+        self.game_started = False
+        self.init_screen()
 
     def keyPressEvent(self, event):
         key = event.key()
+
+        # starting game by button
+        if not self.game_started:
+            if key == event.key():
+                self.game_started = True
+                self.scene().clear()
+                self.start_game()
+
         if key in (Qt.Key_Left, Qt.Key_Right, Qt.Key_Up, Qt.Key_Down):
             # päivitetään suunta vain jos se ei ole vastakkainen valitulle suunnalle
             if key == Qt.Key_Left and self.direction != Qt.Key_Right:
@@ -59,6 +68,12 @@ class SnakeGame(QGraphicsView):
             self.snake.pop()  
 
         self.print_game()
+
+    def init_screen(self):
+        start_text = self.scene().addText("Press any key to start", QFont("Arial", 18))
+        text_width = start_text.boundingRect().width()
+        text_x = (self.width() - text_width) / 5
+        start_text.setPos(text_x, GRID_HEIGHT * CELL_SIZE / 2)
 
     def spawn_food(self):
        
